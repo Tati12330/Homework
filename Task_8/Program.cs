23d6ce70a926307703ccdee6d3374bb374fac27d
Tatka <tatka12330@mail.ru>
2022-08-09 02:57:46 +0400

    Homework Task 08

---
+++ Task_8/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+if (N > 0)
+{
+    while (count <= N)
+    {
+
+        Console.WriteLine(count + " ");
+        count += 2;
+    }
+}
+
+else
+{
+    Console.WriteLine("Введите положительное число");
+}
