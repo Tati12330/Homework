1fad34c9ef96a89a3137788b346c2cd71aa8be73
Tatka <tatka12330@mail.ru>
2022-08-18 01:04:03 +0400

    Homework Sem3 Task 19

---
+++ Task_19/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < -9999)
+{
+    number = - number;
+}
+
+string numberStr = Convert.ToString(number);
+
+if (numberStr.Length == 5)
+{
+  if (numberStr[0] == numberStr[4] & numberStr[1]== numberStr[3])
+  {
+    Console.WriteLine("Да, это палиндром.");
+  }
+  else
+  {
+    Console.WriteLine("Нет, это не палиндром.");
+  }
+}
+else
+{
+    Console.WriteLine("Это не пятизначное число. Повторите ввод.");
+}
