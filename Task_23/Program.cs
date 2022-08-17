3d9ce4bd36a1273e2adaf1ec672f301de2dffb7f
Tatka <tatka12330@mail.ru>
2022-08-18 03:10:20 +0400

    Homework Sem3 Task 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27 
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"Таблица кубов чисел от 1 до {N}: ");
+Console.WriteLine();
+Console.WriteLine("Число\tКуб числа");
+    
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine("{0}\t{1}", i, Math.Pow(i, 3));
+}
