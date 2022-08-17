4222330e7c1eec50e2b21008ae81dc499a46bd93
Tatka <tatka12330@mail.ru>
2022-08-18 00:16:20 +0400

    Homework Sem2 Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 & number < 1000)
+{
+    Console.WriteLine((number % 100) / 10);
+}
+else
+{
+    Console.WriteLine("Это не трехзначное число.");
+}
