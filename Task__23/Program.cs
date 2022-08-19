8070453bb35ff48a9e95ddf86954a73760fc02fe
Tatka <tatka12330@mail.ru>
2022-08-20 01:23:29 +0400

    Homework seminar 3 Task 23

---
+++ Task__23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27 
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Таблица кубов чисел от 1 до N:");
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 3) + " ");
+    }
+}
+else
+{
+    Console.WriteLine("Введите, пожалуйста, положительное число с новой попыткой.");
+}
