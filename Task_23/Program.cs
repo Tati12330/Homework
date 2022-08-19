db40d7ce5ab79b18eb8f15f2b00380654f813b07
Tatka <tatka12330@mail.ru>
2022-08-20 01:05:30 +0400

    Homework Sem3 Task 23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -2,15 +2,17 @@
 // и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27 
 
-Console.Write("Введите число N: ");
+Console.WriteLine("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine();
-Console.WriteLine($"Таблица кубов чисел от 1 до {N}: ");
-Console.WriteLine();
-Console.WriteLine("Число\tКуб числа");
-    
-for (int i = 1; i <= N; i++)
+Console.WriteLine("Таблица кубов чисел от 1 до N:");
+if (N > 0)
 {
-    Console.WriteLine("{0}\t{1}", i, Math.Pow(i, 3));
+    for (int i = 1; i <= N; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 3) + " ");
+    }
+}
+else
+{
+    Console.WriteLine("Введите, пожалуйста, положительное число с новой попыткой.");
 }
