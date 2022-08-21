6882b430117842dafcf9519e6457a058b647fd44
Tatka <tatka12330@mail.ru>
2022-08-21 22:10:10 +0400

    Homework Seminar 4 Task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. Вводим массив через консоль
+
+int [] array = new int [8];
+Console.WriteLine("Введите восемь чисел через enter: ");
+
+for (int i = 0; i < array.Length; i ++)
+{
+ array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Массив из введенных вами чисел: ");
+Console.WriteLine(string.Join(", ", array));
