cf68035767c1c07d6ed4d8704875cc22711e5ccc
Tatka <tatka12330@mail.ru>
2022-08-23 01:27:16 +0400

    Homework Seminar 5 Task 38

---
+++ Task38/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = { 3, 7, 22, 2, 78 };
+double min = array[0];
+double max = array[0];
+double result = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+result = max - min;
+Console.WriteLine($"Разница между max и min элементами массива {result}");
