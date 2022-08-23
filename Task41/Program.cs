ea39b7bd846bd642e637b8e2ef6b9ee9d1d10f9e
Tatka <tatka12330@mail.ru>
2022-08-24 00:04:54 +0400

    Homework Seminar 6 Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+
+Console.Write("Введи число m (количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+
+for (int i = 0; i < m; i ++)
+{
+    Console.Write($"Введи {i + 1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i ++)
+{
+    if (array[i] > 0)
+    {
+        count ++;
+    }
+}
+Console.WriteLine($"Количество введенных вами чисел больше нуля равно {count}");
